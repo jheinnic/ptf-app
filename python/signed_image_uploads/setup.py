df292fc185c211d8b0d59515ece5023ce42d7b2a
John Heinnickel <john.heinnickel@thermofisher.com>
2022-03-04 23:29:28 -0800

    signed_image python packaging

---
+++ python/signed_image_uploads/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="jchptf_image_upload",
+    version="0.1",
+    packages=['jchptf_image_upload'],
+    description='Signed Image Upload GRPC Service',
+    url='http://github.com/jheinnic/jchptf-app/python/signed_image_uploads',
+    author='John Heinnickel',
+    author_email='jheinnic+ptf@hotmail.com',
+    keywords=['pip','example','cloudinary','grpc','uploads'],
+    zip_safe=True
+)
