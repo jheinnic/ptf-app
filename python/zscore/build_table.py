2693f7274736394d615d38a28bd54f251bdc6594
John Heinnickel <john.heinnickel@thermofisher.com>
2025-12-23 09:03:27 +0000

    Fix zscore construction

--- python/zscore/build_table.py
+++ python/zscore/build_table.py
@@ -1,28 +1,38 @@
-# Import all libraries for this portion of the blog post
 from scipy.integrate import quad
 import numpy as np
+import pandas as pd
 
-x = np.linspace(-4, 4, num = 100)
-constant = 1.0 / np.sqrt(2*np.pi)
-pdf_normal_distribution = constant * np.exp((-x**2) / 2.0)
-fig, ax = plt.subplots(figsize=(10, 5));
+# x = np.linspace(-4, 4, num = 100)
+# constant = 1.0 / np.sqrt(2*np.pi)
+# pdf_normal_distribution = constant * np.exp((-x**2) / 2.0)
+# fig, ax = plt.subplots(figsize=(10, 5));
 
 def normalProbabilityDensity(x):
-    constant = 1.0 / np.sqrt(2*np.pi)
-    return(constant * np.exp((-x**2) / 2.0) )
+    constant = 1.0 / np.sqrt(2 * np.pi)
+    return constant * np.exp((-x**2) / 2.0)
 
-indices = np.round(np.arange(0, 3.5, 1), 2),
-columns = np.round(n.arange(0.00, 0.1, 0.01), 2)
-data = np.ndarray([35, 10])
+# 1. Define row and column headers
+# We want rows to be 0.0, 0.1, 0.2 ... 3.4
+# We want columns to be 0.00, 0.01 ... 0.09
+indices = np.round(np.arange(-3.5, 3.5, 0.1), 2)
+columns = np.round(np.arange(0.00, 0.1, 0.01), 2)
 
-ii = 0
-for index in indices: 
-    jj = 0
-    for column in columns:
-        z = np.round(index + column, 2)
-        value, _ = quad(normalProbabilityDensity, np.NINF, z)
-        data[ii, jj] = value
-        jj = jj + 1
-    ii = ii + 1
+# 2. Initialize the data array (35 rows, 10 columns)
+data = np.zeros((len(indices), len(columns)))
 
+# 3. Fill the table using numerical integration
+for i, row_val in enumerate(indices):
+    for j, col_val in enumerate(columns):
+        z = row_val + col_val
+        # quad returns (integral_value, estimated_error)
+        value, _ = quad(normalProbabilityDensity, -np.inf, z)
+        data[i, j] = value
 
+# 4. Display as a nice DataFrame for readability
+z_table = pd.DataFrame(data, index=indices, columns=columns)
+
+# Set options to show all rows and columns
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000) # Prevents line-wrapping
+print(z_table)
